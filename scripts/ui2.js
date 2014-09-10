d1400ed3bbc7b25411c5875ae582ec8762724bad
Kiriaki Kavouras <kiriaki.kavouras@gmail.com>
2014-09-10 13:25:31 -0400

    Fixed icon display upon expanding tabs & some centering

--- scripts/ui2.js
+++ scripts/ui2.js
@@ -10,7 +10,9 @@ $(document).ready(function(){
 		projects = $("#projects"),
 		tab = $(".tab"),
 		navigation = $(".navigation"),
-		about_me = $(".about_me");
+		about_me = $(".about_me"),
+		content = $(".content"),
+		work = $("#work");
 
 	
 	tab.on("click", function(){
@@ -18,60 +20,123 @@ $(document).ready(function(){
 			current_id = $(this).attr('id');
 
 
-		changeDisplay(about_me, "hidden", 500);
-
-					
-		tab.not($this).toggleClass("hidden");
-
-		$this.toggleClass('expanded');
-
-
+		$this.attr("class", "expanded");
+		tab.not($this).attr("class", "hidden");
 		
 		switch(current_id){
 			case "education":
-				main_container.toggleClass("expanded_left");
-				navigation.toggleClass("expanded_vertically");
-				changeDisplay(education_content, "hidden", 1000);
+				about_me.addClass("hidden");
+				main_container.attr("class", "expanded_left");
+				navigation.removeClass("hidden");
+				navigation.removeClass("expanded_horizontally");
+				navigation.addClass("expanded_vertically");
+				$("#education_nav").addClass("hidden");
+				if(education_content.hasClass("hidden")){
+					setTimeout(function(){
+						education_content.removeClass("hidden");
+					}, 750);
+				};
 				break;
+				
 			case "contact":
-				main_container.toggleClass("expanded_top");
-				navigation.toggleClass("expanded_horizontally");
-				contact_content.toggleClass("hidden");
+				about_me.addClass("hidden");
+				main_container.attr("class", "expanded_top");
+				navigation.removeClass("hidden");
+				navigation.removeClass("expanded_vertically");
+				navigation.addClass("expanded_horizontally");
+				$("#contact_nav").addClass("hidden");
+				contact_content.removeClass("hidden");
 				break;
+				
 			case "projects":
-				main_container.toggleClass('expanded_right');
-				navigation.toggleClass("expanded_vertically");
-				changeDisplay(projects_content, "hidden", 1000);
-//				projects_content.toggleClass("hidden");
+				about_me.addClass("hidden");
+				main_container.attr("class", "expanded_right");
+				navigation.removeClass("hidden");
+				navigation.removeClass("expanded_horizontally");
+				navigation.addClass("expanded_vertically");
+				$("#projects_nav").addClass("hidden");
+				if(projects_content.hasClass("hidden")){
+					setTimeout(function(){
+						projects_content.removeClass("hidden");
+					}, 750);
+				};
 				break;
+			
 			case "work":
-				main_container.toggleClass('expanded_bottom');
-				navigation.toggleClass("expanded_horizontally");
-				work_content.toggleClass("hidden");
+				about_me.addClass("hidden");
+				main_container.attr("class", "expanded_bottom");
+				navigation.removeClass("hidden");
+				navigation.removeClass("expanded_vertically");
+				navigation.addClass("expanded_horizontally");
+				$("#work_nav").addClass("hidden");
+				work_content.removeClass("hidden");
 				break;
+
 		}
-		
 
 
+
+	});
+	
+	$("#home_nav").on("click", function(){
+		main_container.attr("class", "");
+		content.addClass("hidden");
+		tab.removeClass("expanded hidden");
+		navigation.removeClass("expanded_vertically expanded_horizontally");
+		navigation.addClass("hidden");
+		setTimeout(function(){
+			about_me.removeClass("hidden");
+		}, 500);
+	});
+	
+	$("#projects_nav").on("click", function(){
+		expandedTab(projects);
+		main_container.attr("class", "expanded_right");
+		projects_content.removeClass("hidden");
+		navigation.removeClass("hidden");
+		navigation.removeClass("expanded_horizontally");
+		navigation.addClass("expanded_vertically");
+		$("#projects_nav").addClass("hidden");
+	});
+	
+	$("#contact_nav").on("click", function(){
+		main_container.attr("class", "expanded_top");
+		expandedTab(contact);
+		contact_content.removeClass("hidden");
+		navigation.removeClass("hidden");
+		navigation.removeClass("expanded_vertically");
+		navigation.addClass("expanded_horizontally");
+		$("#contact_nav").addClass("hidden");
+	});
+	
+	$("#education_nav").on("click", function(){
+		main_container.attr("class", "expanded_left");
+		expandedTab(education);
+		education_content.removeClass("hidden");
+		navigation.removeClass("hidden");
+		navigation.removeClass("expanded_horizontally");
+		navigation.addClass("expanded_vertically");
+		$("#education_nav").addClass("hidden");
+	});
+	
+	$("#work_nav").on("click", function(){
+		main_container.attr("class", "expanded_bottom");
+		expandedTab(work);
+		work_content.removeClass("hidden");
+		navigation.removeClass("hidden");
+		navigation.removeClass("expanded_vertically");
+		navigation.addClass("expanded_horizontally");
+		$("#work_nav").addClass("hidden"); //$THIS? 
 	});
 
 
 
-//Change content's visibility state
-//If it is visible, hide immediately
-//If content is hidden, delay its appearance by a certain nuber of ms. 
-//IN: target content, class name for hidden state, delay time in ms
-function changeDisplay(target_content, hidden_class_name, delay){
-	if(target_content.hasClass(hidden_class_name)){
-		setTimeout(function(){
-			target_content.toggleClass(hidden_class_name);
-		}, delay);
-	}
-	else{
-		target_content.toggleClass(hidden_class_name);
-	}
+//Makes a certain tab the only one visible by ensuring it is expanded and hiding all the others
+//IN: target tab to be displayed
+function expandedTab(target_tab){
+	tab.not(target_tab).addClass("hidden").removeClass("expanded");
+	target_tab.removeClass("hidden").addClass("expanded");
 }
-	
 	
 	
 });
