be44289f8ded5bb0d56b12393d913511dc131345
Kiriaki Kavouras <kiriaki.kavouras@gmail.com>
2014-11-18 17:15:19 -0500

    Made responsive up to 900px

--- scripts/ui2.js
+++ scripts/ui2.js
@@ -13,7 +13,26 @@ $(document).ready(function(){
 		about_me = $(".about_me"),
 		content = $(".content"),
 		work = $("#work"),
-		$xs_tab = $(".xs-tab");
+		$xs_tab = $(".xs-tab"),
+		$med_content = $("#med_content"),
+		$window = $(window),
+		my_window = new CurrentWindow
+		breakpoints = [0,480,900];
+
+
+// Object keeping track of the width of the window to serve as comparison with current state 
+// Makes it possible to check if a breakpoint jump has occurred
+	function CurrentWindow(){
+		this.last_known_width = $window.width();
+
+		this.storeWidth = function(){
+			this.last_known_width = $window.width();
+		}
+
+		this.getWidth = function(){
+			return this.last_known_width;
+		}
+	}
 
 	
 	tab.on("click", function(){
@@ -130,7 +149,16 @@ $(document).ready(function(){
 		$("#work_nav").addClass("hidden"); //$THIS? 
 	});
 	
-
+	setHandlers($window.width());
+	$window.resize(function(){ 
+		$this = $(this);
+		if(checkBreakpointJump(my_window.getWidth(), $this.width(), breakpoints)){
+			$xs_tab.off();
+			main_container.off();
+			setHandlers($this.width());
+			my_window.storeWidth();
+		};
+	});
 
 
 	//Makes a certain tab the only one visible by ensuring it is expanded and hiding all the others
@@ -140,56 +168,199 @@ $(document).ready(function(){
 		target_tab.removeClass("hidden").addClass("expanded");
 	}
 
-	///////////////////////////////////////////////////////////////////////////////////////////////////////	
-	// // media query event handler
-	if (matchMedia) {
-		var mq = window.matchMedia("(max-width: 800px)");
-		mq.addListener(WidthChange);
-		WidthChange(mq);
-	}
+	//Sets event handlers for elements based on the size of the window
+	//IN: width in pixels of current display window
+	//OUT:--
 
-// media query change
-	function WidthChange(mq) {
-
-		if (mq.matches) {
-			// window width is up to 480px
+	//If current window width jumps a breakpoint change the handlers accordingly
+	function setHandlers(window_width){
+		if(window_width<=480){
+			if(!$("#home_nav").hasClass("hidden")){
+					$("#home_nav").click();
+			}
 			$("#main_container").on("click", function(){
 				$(this).toggleClass("expanded");
 			});
+			//Handles the clicking on xs-tab
+			$xs_tab.on("click", function(){
+
+				$("#med_content").addClass("G");
+				var $this = $(this);
+				
+				$this.toggleClass("expanded");
+				$this.children(".fa:first")
+					.toggleClass("expanded");
+				$this.children("div:first")  
+					.toggleClass("hidden");
+				$this.children(".xs_content")
+					.toggleClass("hidden");
+
+				$xs_tab.not($this)	
+					.removeClass("expanded")
+					.children("div")
+					.addClass("hidden");
+
+				$xs_tab.not($this)
+					.children(".fa")
+					.removeClass("expanded");
+
+				if($this.hasClass("expanded")){
+					$('html, body').animate({
+			      	  scrollTop: $this.offset().top
+			   		}, 500);
+				}
+
+			});
+		} 
+		else if(window_width<=900){
+			main_container.on("click", function(){
+				$(this).toggleClass("expanded");
+			});
+
+			$xs_tab.on("click", function(){
+				var $this = $(this);
+				$med_content.children(".xs_content").remove();
+				$med_content.append($this.children(".xs_content").clone());
+
+				$med_content.removeClass();
+				if($this.hasClass("expanded")){
+					$med_content.addClass("hidden");
+					$med_content.children(".xs_content").remove();
+				}
+
+				switch($this.attr("id")){
+					case "contact_xs":
+						$med_content.addClass("expanded_contact");
+						break;
+					case "projects_xs":
+						$med_content.addClass("expanded_projects");
+						break;
+					case "work_xs":
+						$med_content.addClass("expanded_work");
+						break;
+					case "education_xs":
+						$med_content.addClass("expanded_education");
+						break;
+				}
+				$this.children(".fa").toggleClass("expanded");
+				$xs_tab.not($this)
+					.removeClass("expanded")
+					.children(".fa")
+					.removeClass("expanded");
+				$this.toggleClass("expanded");
+				$med_content.children().removeClass("hidden");
+
+
+			});
 		}
-		else {
-			$("#main_container").off("click");
-			// window width is > 480px
-			mq.removeListener()
+		else{
 		}
+
 	}
 
-	//Handles the clicking on xs-tab(<=480)
-	$xs_tab.on("click", function(){
-		var $this = $(this);
+
+
+// 	///////////////////////////////////////////////////////////////////////////////////////////////////////	
+// 	// // media query event handler
+// 	if (matchMedia) {
+// 		var mql = window.matchMedia("(max-width: 480px)"),
+// 			mql_med = window.matchMedia("(min-width:481px) and (max-width:801px)"),
+// 			mql_large = window.matchMedia("(min-width:801px");
+// 		mql.addListener(onWidthChange);
+// 		mql_med.addListener(onWidthChange);
+// 		mql_large.addListener(onWidthChange);
+// 		onWidthChange(mql, mql_med, mql_large);
+// 	}
+
+// // media query change
+// 	function onWidthChange(mql, bb, mql_large) {
+
+// 		if (mql.matches) {	// window width is up to 480px
+// 			if(!$("#home_nav").hasClass("hidden")){
+// 					$("#home_nav").click();;
+// 			}
+// 			$("#main_container").on("click", function(){
+// 				$(this).toggleClass("expanded");
+// 			});
+// 			console.log("small");
+// 			tab.addClass("hidden");
+// 		}
+// 		console.log(mql);
+// 		if (bb.matches){
+
+// 		}
+// 		// console.log(bb);
+// 		// console.log(mql_large);
+// 		// else if (bb.matches){ //window between 481px and 800px
+// 		// 	console.log("MED");
+// 		// 	$xs_tab.on("click", function(){
+// 		// 		$med_content.toggleClass("G");
+// 		// 	});
+// 		// }
+// 		// else if(mql_large.matches){			// window width is > 801px
+// 		// 	$("#main_container").off("click");
+// 		// 	tab.removeClass("hidden");
+// 		// 	console.log("big");
+// 		// 	// mql.removeListener()
+// 		// 	// mql_med.removeListener();
+// 		// }
+// 	}
+
+	// //Handles the clicking on xs-tab
+	// $xs_tab.on("click", function(){
+
+	// 	$("#med_content").addClass("G");
+	// 	var $this = $(this);
 		
-		$this.toggleClass("expanded");
-		$this.children(".fa:first")
-			.toggleClass("expanded");
-		$this.children("div:first")  //CAN I COMMA SEPARATE OR SOMETHING??
-			.toggleClass("hidden");
-		$this.children(".xs_content_container")
-			.toggleClass("hidden");
-		if($this.hasClass("expanded")){
-			$('html, body').animate({
-	      	  scrollTop: $this.offset().top
-	   		}, 500);
-		}
-		$xs_tab.not($this)	
-			.removeClass("expanded")
-			.children("div")
-			.addClass("hidden");
+	// 	$this.toggleClass("expanded");
+	// 	$this.children(".fa:first")
+	// 		.toggleClass("expanded");
+	// 	$this.children("div:first")  
+	// 		.toggleClass("hidden");
+	// 	$this.children(".xs_content_container")
+	// 		.toggleClass("hidden");
 
-		$xs_tab.not($this)
-			.children(".fa")
-			.removeClass("expanded");
+	// 	$xs_tab.not($this)	
+	// 		.removeClass("expanded")
+	// 		.children("div")
+	// 		.addClass("hidden");
 
-	});
+	// 	$xs_tab.not($this)
+	// 		.children(".fa")
+	// 		.removeClass("expanded");
+
+	// 	if($this.hasClass("expanded")){
+	// 		$('html, body').animate({
+	//       	  scrollTop: $this.offset().top
+	//    		}, 500);
+	// 	}
+
+	// });
 
 	
 });
+
+	//Checks if the width is changing between breakpoints
+	//IN:breakpoints in pixels
+	//OUT:boolean indicating whether a breakpoint was jumped
+	function checkBreakpointJump(width_before, width_after, breakpoints){
+		for(var i=0; i<breakpoints.length; i++){
+			if((width_before<=breakpoints[i] && width_after>breakpoints[i]) || (width_before>breakpoints[i] && width_after<=breakpoints[i])){
+				console.log("JUMP!");
+				return true;
+			}
+		}
+		return false;
+
+	}
+
+	//Moves DOM elements from one parent to another
+	//IN:  target JQUERY DOM element to be moved,
+	//	   JQUERY parent to which to append target DOM element
+	//OUT:--
+	function relocate(target_element, new_parent){
+		var my_element = target_element.remove();
+		new_parent.append(my_element);
+	}
+
+
