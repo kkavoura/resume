1c13764cfa3a91e56c8c9c0b7c8ec94521d25331
Kiriaki Kavouras <kiriaki.kavouras@gmail.com>
2014-11-25 13:47:25 -0500

    Total overhaul of main page, removed old/unused html, js and css. Hexagons rotate on hover, not functional yet

--- scripts/ui2.js
+++ scripts/ui2.js
@@ -10,14 +10,16 @@ $(document).ready(function(){
 		projects = $("#projects"),
 		tab = $(".tab"),
 		navigation = $(".navigation"),
-		about_me = $(".about_me"),
 		content = $(".content"),
 		work = $("#work"),
 		$xs_tab = $(".xs-tab"),
 		$med_content = $("#med_content"),
 		$window = $(window),
-		my_window = new CurrentWindow
-		breakpoints = [0,480,900];
+		my_window = new CurrentWindow,
+		breakpoints = [0,480,900],
+		$hexagon = $(".hexagon");
+
+		// $hexagon.on("click");
 
 
 // Object keeping track of the width of the window to serve as comparison with current state 
@@ -34,120 +36,9 @@ $(document).ready(function(){
 		}
 	}
 
-	
-	tab.on("click", function(){
-		var $this = $(this),
-			current_id = $(this).attr('id');
-
-
-		$this.attr("class", "expanded");
-		tab.not($this).attr("class", "hidden");
-		
-		switch(current_id){
-			case "education":
-				about_me.addClass("hidden");
-				main_container.attr("class", "expanded_left");
-				navigation.removeClass("hidden");
-				navigation.removeClass("expanded_horizontally");
-				navigation.addClass("expanded_vertically");
-				$("#education_nav").addClass("hidden");
-				if(education_content.hasClass("hidden")){
-					setTimeout(function(){
-						education_content.removeClass("hidden");
-					}, 750);
-				};
-				break;
-				
-			case "contact":
-				about_me.addClass("hidden");
-				main_container.attr("class", "expanded_top");
-				navigation.removeClass("hidden");
-				navigation.removeClass("expanded_vertically");
-				navigation.addClass("expanded_horizontally");
-				$("#contact_nav").addClass("hidden");
-				contact_content.removeClass("hidden");
-				break;
-				
-			case "projects":
-				about_me.addClass("hidden");
-				main_container.attr("class", "expanded_right");
-				navigation.removeClass("hidden");
-				navigation.removeClass("expanded_horizontally");
-				navigation.addClass("expanded_vertically");
-				$("#projects_nav").addClass("hidden");
-				if(projects_content.hasClass("hidden")){
-					setTimeout(function(){
-						projects_content.removeClass("hidden");
-					}, 750);
-				};
-				break;
-			
-			case "work":
-				about_me.addClass("hidden");
-				main_container.attr("class", "expanded_bottom");
-				navigation.removeClass("hidden");
-				navigation.removeClass("expanded_vertically");
-				navigation.addClass("expanded_horizontally");
-				$("#work_nav").addClass("hidden");
-				work_content.removeClass("hidden");
-				break;
-
-		}
 
 
 
-	});
-	
-	$("#home_nav").on("click", function(){
-		main_container.attr("class", "");
-		content.addClass("hidden");
-		tab.removeClass("expanded hidden");
-		navigation.removeClass("expanded_vertically expanded_horizontally");
-		navigation.addClass("hidden");
-		setTimeout(function(){
-			about_me.removeClass("hidden");
-		}, 500);
-	});
-	
-	$("#projects_nav").on("click", function(){
-		expandedTab(projects);
-		main_container.attr("class", "expanded_right");
-		projects_content.removeClass("hidden");
-		navigation.removeClass("hidden");
-		navigation.removeClass("expanded_horizontally");
-		navigation.addClass("expanded_vertically");
-		$("#projects_nav").addClass("hidden");
-	});
-	
-	$("#contact_nav").on("click", function(){
-		main_container.attr("class", "expanded_top");
-		expandedTab(contact);
-		contact_content.removeClass("hidden");
-		navigation.removeClass("hidden");
-		navigation.removeClass("expanded_vertically");
-		navigation.addClass("expanded_horizontally");
-		$("#contact_nav").addClass("hidden");
-	});
-	
-	$("#education_nav").on("click", function(){
-		main_container.attr("class", "expanded_left");
-		expandedTab(education);
-		education_content.removeClass("hidden");
-		navigation.removeClass("hidden");
-		navigation.removeClass("expanded_horizontally");
-		navigation.addClass("expanded_vertically");
-		$("#education_nav").addClass("hidden");
-	});
-	
-	$("#work_nav").on("click", function(){
-		main_container.attr("class", "expanded_bottom");
-		expandedTab(work);
-		work_content.removeClass("hidden");
-		navigation.removeClass("hidden");
-		navigation.removeClass("expanded_vertically");
-		navigation.addClass("expanded_horizontally");
-		$("#work_nav").addClass("hidden"); //$THIS? 
-	});
 	
 	setHandlers($window.width());
 	$window.resize(function(){ 
@@ -183,6 +74,11 @@ $(document).ready(function(){
 
 	//If current window width jumps a breakpoint change the handlers accordingly
 	function setHandlers(window_width){
+		if(window_width<900){
+			console.log("small");
+			main_container.removeClass("hidden");
+		}
+
 		if(window_width<=480){
 			if(!$("#home_nav").hasClass("hidden")){
 					$("#home_nav").click();
@@ -258,94 +154,13 @@ $(document).ready(function(){
 					.removeClass("expanded");
 				$this.toggleClass("expanded");
 				$med_content.children().removeClass("hidden");
-
-
 			});
 		}
 		else{
+			main_container.addClass("hidden");
 		}
 
 	}
-
-
-
-// 	///////////////////////////////////////////////////////////////////////////////////////////////////////	
-// 	// // media query event handler
-// 	if (matchMedia) {
-// 		var mql = window.matchMedia("(max-width: 480px)"),
-// 			mql_med = window.matchMedia("(min-width:481px) and (max-width:801px)"),
-// 			mql_large = window.matchMedia("(min-width:801px");
-// 		mql.addListener(onWidthChange);
-// 		mql_med.addListener(onWidthChange);
-// 		mql_large.addListener(onWidthChange);
-// 		onWidthChange(mql, mql_med, mql_large);
-// 	}
-
-// // media query change
-// 	function onWidthChange(mql, bb, mql_large) {
-
-// 		if (mql.matches) {	// window width is up to 480px
-// 			if(!$("#home_nav").hasClass("hidden")){
-// 					$("#home_nav").click();;
-// 			}
-// 			$("#main_container").on("click", function(){
-// 				$(this).toggleClass("expanded");
-// 			});
-// 			console.log("small");
-// 			tab.addClass("hidden");
-// 		}
-// 		console.log(mql);
-// 		if (bb.matches){
-
-// 		}
-// 		// console.log(bb);
-// 		// console.log(mql_large);
-// 		// else if (bb.matches){ //window between 481px and 800px
-// 		// 	console.log("MED");
-// 		// 	$xs_tab.on("click", function(){
-// 		// 		$med_content.toggleClass("G");
-// 		// 	});
-// 		// }
-// 		// else if(mql_large.matches){			// window width is > 801px
-// 		// 	$("#main_container").off("click");
-// 		// 	tab.removeClass("hidden");
-// 		// 	console.log("big");
-// 		// 	// mql.removeListener()
-// 		// 	// mql_med.removeListener();
-// 		// }
-// 	}
-
-	// //Handles the clicking on xs-tab
-	// $xs_tab.on("click", function(){
-
-	// 	$("#med_content").addClass("G");
-	// 	var $this = $(this);
-		
-	// 	$this.toggleClass("expanded");
-	// 	$this.children(".fa:first")
-	// 		.toggleClass("expanded");
-	// 	$this.children("div:first")  
-	// 		.toggleClass("hidden");
-	// 	$this.children(".xs_content_container")
-	// 		.toggleClass("hidden");
-
-	// 	$xs_tab.not($this)	
-	// 		.removeClass("expanded")
-	// 		.children("div")
-	// 		.addClass("hidden");
-
-	// 	$xs_tab.not($this)
-	// 		.children(".fa")
-	// 		.removeClass("expanded");
-
-	// 	if($this.hasClass("expanded")){
-	// 		$('html, body').animate({
-	//       	  scrollTop: $this.offset().top
-	//    		}, 500);
-	// 	}
-
-	// });
-
 	
 });
 
